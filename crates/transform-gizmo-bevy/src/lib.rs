f7258504fa8366b32884896a4992855e1416aee0
Shenmarukai <shanemulc@comcast.net>
2024-09-09 07:25:20 -0400

    test

--- crates/transform-gizmo-bevy/src/lib.rs
+++ crates/transform-gizmo-bevy/src/lib.rs
@@ -508,7 +508,7 @@ fn update_gizmos(
             };
 
             let rotation = target_transform.rotation * ( DQuat::from(result_transform.rotation).as_quat().inverse() * target_global_transform.rotation ).inverse();
-            target_transform.translation += rotation * DVec3::from(result_transform.translation).as_vec3() - target_global_transform.translation;
+            target_transform.translation += rotation * ( DVec3::from(result_transform.translation).as_vec3() - target_global_transform.translation );
             target_transform.rotation = rotation;
             target_transform.scale = DVec3::from(result_transform.scale).as_vec3();
         }
